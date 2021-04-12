40b6534b1ebdf789105673ebda7bc151addf443d
Sir-Quack-III <77940766+Sir-Quack-III@users.noreply.github.com>
2021-04-12 11:41:48 -0700

    Added basic CPU stuff

--- python/main.py
+++ python/main.py
@@ -1,1 +1,20 @@
-{\rtf1}
+class Mem:
+	RAM = [] * 128
+	iCache = [] * 256
+	def init(self):
+		for x in RAM:
+			RAM[x] = 0x00;
+
+class CPU:
+	mem = Mem()
+
+	registers = [0x00] * 32
+
+	SP = 0x00
+	PC = 0x00
+
+	def init(self):
+		mem.init()
+
+if __name__ == "__main__":
+	cpu = CPU()
